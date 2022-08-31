0b6ec0fa0dc23cc338845defde097f198a50907f
MMA <lesnic1987@mail.ru>
2022-08-31 22:40:11 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+string returnPOW()
+{
+    Console.Write("Введите число ");
+    int x = Convert.ToInt32(Console.ReadLine());
+    int start = 1;
+    string res = string.Empty; //string.Empty - пустая строка
+    while (start <= x)
+    {
+        res = res + start*start*start+", ";
+        start++;
+    }
+    return res;
+}
+try
+{
+    Console.WriteLine(returnPOW());
+}
+catch
+{
+    Console.WriteLine("Необходимо вводить числа");
+}
